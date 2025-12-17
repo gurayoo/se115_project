92150acfd69a6b5f7d02a7d563c97a1162a2de27
gurayoo <ozturk.guray07@gmail.com>
2025-12-17 13:52:17 +0300

    method 3 and 4

--- src/Main.java
+++ src/Main.java
@@ -16,4 +16,32 @@ public class Main {
         }
         return -1;
     }
+
+    public static String mostProfitableCommodityInMonth(int month) {
+        if (month < 0 || month >= MONTHS) return "INVALID_MONTH";
+        int maxProfit = Integer.MIN_VALUE;
+        int bestCommodityIndex = -1;
+        for (int c = 0; c < COMMS; c++) {
+            int currentTotal = 0;
+            for (int d = 0; d < DAYS; d++) {
+                currentTotal += profitData[month][d][c];
+            }
+            if (currentTotal > maxProfit) {
+                maxProfit = currentTotal;
+                bestCommodityIndex = c;
+            }
+        }
+        if (bestCommodityIndex == -1) return "No Data 0";
+        return commodities[bestCommodityIndex] + " " + maxProfit;
+    }
+
+    public static int totalProfitOnDay(int month, int day) {
+        if (month < 0 || month >= MONTHS || day < 1 || day > DAYS) return -99999;
+        int dayIndex = day - 1;
+        int total = 0;
+        for (int c = 0; c < COMMS; c++) {
+            total += profitData[month][dayIndex][c];
+        }
+        return total;
+    }
 }
